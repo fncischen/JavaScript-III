955ea2902ca6dbca9b66e934df2d6336184e05f6
Francis Chen <fncis.chen@gmail.com>
2019-01-23 14:02:20 -0800

    initial commit

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,14 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* SUMMARY:  The value of "this" is
+*  referencing the scope of function is executing the this keyword.
+*
+* 1. Implicit binding uses the dot notation (this.) to reference the nearest object scope / execution context of the function. 
+* 2. Explicit binding allows for the clear, explicit defining of "this", since both call() and apply() methods allow direct
+* reference the input arguments to the object you want to bind those arguments/properties to. You are clearly defining the execution context of a function.
+* 3. When a constructor function is used, this refers to the object that the constructor function is creating. 
+* 4. When a object method is used, this refers to the object that the method derives itself from. 
 *
 * write out a code example of each explanation above
 */
